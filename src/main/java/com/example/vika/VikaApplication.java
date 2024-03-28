273c4e4daf8b94f84a7747a0e94c527d074dc8c7
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-03-29 01:54:29 +0530

    nav

--- src/main/java/com/example/vika/VikaApplication.java
+++ src/main/java/com/example/vika/VikaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VikaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VikaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VikaApplication.class, args);
+    }
 
 }
--- src/main/java/com/example/vika/controller/MainController.java
+++ src/main/java/com/example/vika/controller/MainController.java
@@ -1,2 +1,13 @@
-package com.example.vika.controller;public class MainController {
+package com.example.vika.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class MainController {
+    @GetMapping("/")
+    public String homePage(){
+        return "home";
+    }
 }
