fefdd4e1b9261f7572d8cef7fb40b9d25f9d6983
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-03-29 04:18:41 +0530

    add Bd + controller +

--- src/main/resources/Contact.sql
+++ src/main/resources/Contact.sql
@@ -1,7 +1,7 @@
 CREATE TABLE contact (
                          id bigserial NOT NULL,
                          email varchar(255),
-                         message varchar(255),
+                         message varchar(500),
                          name varchar(255),
                          phone varchar(255),
                          PRIMARY KEY (id)
