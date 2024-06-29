f50b51c10ad45caa618e86d3b590cf85133dfc57
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-06-30 01:00:15 +0300

    save bag register end  post metod

--- src/main/java/com/example/vika/config/Config.java
+++ src/main/java/com/example/vika/config/Config.java
@@ -7,7 +7,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -20,18 +25,24 @@ public class Config {
 
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(req->req.
-                requestMatchers("/").permitAll()
-                        .requestMatchers("/home.css/**").permitAll()
-                        .requestMatchers("/pic/**").permitAll()
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(aut->aut.requestMatchers("/","/pic/**","/home.css/**","/Contact.css/**","/addContact/**").permitAll()
+
+                        .requestMatchers("/contact/**").authenticated())
 
 
-                        .requestMatchers("/contact").authenticated()
-                .anyRequest().authenticated())
+                .formLogin((form->form.loginPage("/login").permitAll()))
+                .logout((log->log.permitAll()))
 
-                .formLogin(form->form.loginPage("/login").permitAll());
-//                .logout(log->log.permitAll());
-        return http.build();
+                .build();
+    }
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+                .username("q")
+                .password("q")
+                .build();
 
+        return new InMemoryUserDetailsManager(userDetails);
     }
 }
--- src/main/java/com/example/vika/controller/MainController.java
+++ src/main/java/com/example/vika/controller/MainController.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @AllArgsConstructor
@@ -19,15 +20,19 @@ public class MainController {
     public String homePage(){
         return "home";
     }
+
     @GetMapping("/contact")
     public String getAllContact(Model model){
+
         model.addAttribute("contact",contacservive.contactList());
         return "contact";
     }
-    @PostMapping("/contact")
-    public String saveContact(@ModelAttribute Contact contact){
+    @PostMapping("/addContact")
+    public String saveContact(@ModelAttribute Contact contact ) {
+
     contacservive.saveContact(contact);
-        return "home";
+//    redirectAttributes.addFlashAttribute("sucsec","спасибо за регистарцию");
+        return "redirect:/";
     }
     @RequestMapping("/contact/{id}")
     public String deleteFromList(@PathVariable (name = "id") Long id){
